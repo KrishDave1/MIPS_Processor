7a6f544336e75032979ed2553504517f1f0f7ce2
KrishDave1 <116882111+KrishDave1@users.noreply.github.com>
2023-11-05 23:20:45 +0530

    Add files via upload

--- MIPS_Non-Pipelining-Processor.py
+++ MIPS_Non-Pipelining-Processor.py
@@ -1,6 +1,6 @@
 from Task1 import dave_list,InstructionHashmap,listOfInstructions
 
-Main_memory = [6,2,4,3,1]
+Main_memory = [67,38,45,15,0]
 
 # Initializing registers
 Register_Array = []
@@ -20,31 +20,21 @@ Register_Array.append(['t5', 0])
 Register_Array.append(['t8', 0])
 Register_Array.append(['t9', 0])
 
-
 # Final Array of Registers
-
-def Jumpinstructions(loop_name, instrDictionary, PC_list):
+def Jumpinstructions(loop_name, PC_list):
     temp_index = 0
-    if loop_name == 'end_sort':
-        exit()
+    # if loop_name == '1048605':
+    #     loop_name = 'loop4'
     for i in range(len(PC_list)):
         if PC_list[i][0] == loop_name:
             temp_index = i
             break
     starting_address = PC_list[temp_index][1][0]
-    print(starting_address)
-    # end_address = PC_list[temp_index][1][1]
-    end_address = 4194376 + len(instrDictionary)*4
-    print(end_address)
-    for i in range(starting_address, end_address, 4):
-        instruction = instrDictionary[i]
-        print(instruction)
-        Execute_Phase(instruction, instrDictionary, PC_list)
+    return starting_address
 
-def Execute_Phase(instruction, instrDictionary, PC_list):
-
-    PC_address = 0
+def Execute_Phase(instruction, PC_list,PC_address):
     if instruction[0] == 'move':
+        PC_address += 4
         temp_index = 0
         for i in range(len(Register_Array)):
             if Register_Array[i][0] == instruction[1]:
@@ -55,6 +45,7 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
                 Register_Array[temp_index][1] = Register_Array[i][1]
                 break
     elif instruction[0] == 'addi':
+        PC_address += 4
         temp_index = 0
         for i in range(len(Register_Array)):
             if Register_Array[i][0] == instruction[1]:
@@ -64,7 +55,21 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
             if Register_Array[i][0] == instruction[2]:
                 Register_Array[temp_index][1] = Register_Array[i][1] + int(instruction[3])
                 break
+    elif instruction[0] == 'add':
+        PC_address += 4
+        temp_index = 0
+        temp_index_1 = 0
+        for i in range(len(Register_Array)):
+            if Register_Array[i][0] == instruction[1]:
+                temp_index = i
+            if Register_Array[i][0] == instruction[2]:
+                temp_index_1 = i
+        for i in range(len(Register_Array)):
+            if Register_Array[i][0] == instruction[3]:
+                Register_Array[temp_index][1] = Register_Array[temp_index_1][1] + Register_Array[i][1]
+                break
     elif instruction[0] == 'sub':
+        PC_address += 4
         temp_index = 0
         temp_index_1 = 0
         for i in range(len(Register_Array)):
@@ -77,6 +82,7 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
                 Register_Array[temp_index][1] = Register_Array[temp_index_1][1] - Register_Array[i][1]
                 break
     elif instruction[0] == 'lw': 
+        PC_address += 4
         # inst ,reg, imm, reg
         temp_index = 0
         for i in range(len(Register_Array)):
@@ -88,6 +94,7 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
                 Register_Array[temp_index][1] = Main_memory[(Register_Array[i][1] + int(instruction[2]))//4]
                 break
     elif instruction[0] == 'sw':
+        PC_address += 4
         temp_index = 0
         for i in range(len(Register_Array)):
             if Register_Array[i][0] == instruction[1]:
@@ -99,6 +106,7 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
                 Main_memory[new] = Register_Array[temp_index][1]
                 break
     elif instruction[0] == 'slt':
+        PC_address += 4
         temp_index = 0
         temp_index_1 = 0
         for i in range(len(Register_Array)):
@@ -122,7 +130,9 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
             if Register_Array[i][0] == instruction[2]:
                 temp_index_1 = i
         if Register_Array[temp_index][1] != Register_Array[temp_index_1][1]:
-            Jumpinstructions(instruction[3], instrDictionary, PC_list)
+            PC_address = Jumpinstructions(instruction[3], PC_list)
+        else:
+            PC_address += 4
     elif instruction[0] == 'beq':
         temp_index = 0
         temp_index_1 = 0
@@ -132,19 +142,28 @@ def Execute_Phase(instruction, instrDictionary, PC_list):
             if Register_Array[i][0] == instruction[2]:
                 temp_index_1 = i
         if Register_Array[temp_index][1] == Register_Array[temp_index_1][1]:
-            Jumpinstructions(instruction[3], instrDictionary, PC_list)
+            PC_address = Jumpinstructions(instruction[3], PC_list)
+        else:
+            PC_address += 4
     elif instruction[0] == 'j':
-        Jumpinstructions(instruction[1], instrDictionary, PC_list)
+        PC_address = Jumpinstructions(instruction[1], PC_list)
     else:
         print("Wrong instruction")
-
-# print(listOfInstructions)
+    return PC_address
 
 PC_list = dave_list
 for i in range(len(PC_list)):
     PC_list[i][1][0] = int(PC_list[i][1][0])
 
-for i in range(len(listOfInstructions)):
-    Execute_Phase(listOfInstructions[i], InstructionHashmap, PC_list)
-    print()
-    # print(Register_Array)
+#For Sorting uncomment this
+# PC_address = 4194380
+# while PC_address < 4194516:
+#     # print(str(PC_address) + " " + str(InstructionHashmap[PC_address]))
+#     PC_address = Execute_Phase(InstructionHashmap[PC_address],PC_list,PC_address)
+# print(Main_memory)
+
+# For Fibonacci uncomment this
+PC_address = 4194304
+while PC_address < 4194340:
+    PC_address = Execute_Phase(InstructionHashmap[PC_address],PC_list,PC_address)
+print(Register_Array[10][0] + " " + str(Register_Array[10][1])) # The answer of fibonacci is stored in t4.
